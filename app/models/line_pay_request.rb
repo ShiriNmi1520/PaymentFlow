b62430945d698bcb70bf6304b09112ce95a8e4a6
Y.C.Huang <s20026352@gmail.com>
2022-09-11 18:30:25 +0800

    feat: Line pay model Add model attribute for package and redirect url

--- app/models/line_pay_request.rb
+++ app/models/line_pay_request.rb
@@ -1,25 +1,54 @@
 require 'uri'
+
+# Model for request payment
 class LinePay < ActiveRecord::Base
-  validate :validate_currency, :validate_url
+  validate :validate_currency
+  validates_length_of :orderId, maximum: 100, too_long: 'ORDER_ID_TOO_LONG'
+
   attribute :amount, :integer, null: false
   attribute :currency, :string, null: false
   attribute :orderId, :string, null: false
   attribute :packages, :LinePayPackages, null: false
-  attribute :redirectUrls, :object, null: false
+  attribute :redirectUrls, :LinePayRedirectUrls, null: false
 
   def validate_currency
-    if currency not (Money::Currency.table.values.map {})
-      errors.add(:currency, "INVALID_CURRENCY_PASSED")
-    end
+    errors.add(:currency, 'INVALID_CURRENCY_PASSED') if currency not (Money::Currency.table.values.map {})
   end
 
-  def validate_url
-    if redirectUrls !=~ URI::regexp
-      errors.add(:redirectUrls, "INVALID_REDIRECT_URL")
-    end
-  end
 end
 
+# Model for packages in request body (Item name, price, etc.)
 class LinePayPackages < ActiveRecord::Base
+  validates_length_of :packageId, maximum: 50, too_long: 'PACKAGE_ID_TOO_LONG'
+  validates_length_of :packageName, maximum: 100, too_long: 'PACKAGE_NAME_TOO_LONG'
 
+
+  attribute :packageId, :string, null: false
+  attribute :packageAmount, :integer, null: false
+  attribute :userFee, :integer
+  attribute :packageName, :string
+  attribute :products, :LinePayPackagesProduct, null: false
+end
+
+class LinePayPackagesProduct < ActiveRecord::Base
+  attribute :productName, :string, null: false
+  attribute :productImageUrl, :string
+  attribute :productQuantity, :integer, null: false
+  attribute :productPrice, :integer, null: false
+  attribute :productOriginalPrice, :integer
+
+end
+
+# Model for redirect urls in request body (Confirm, cancel)
+class LinePayRedirectUrls < ActiveRecord::Base
+  validate :validate_url
+
+  attribute :appPackageName, :string # For Android environment
+  attribute :confirmUrl, :string, null: false
+  attribute :confirmUrlType, :string
+  attribute :cancelUrl, :string, null: false
+  def validate_url
+    errors.add(:confirmUrl, 'INVALID_REDIRECT_URL') if confirmUrl !=~ URI::regexp
+    errors.add(:cancelUrl, 'INVALID_CANCEL_URL') if cancelUrl !=~ URI::regexp
+  end
 end
